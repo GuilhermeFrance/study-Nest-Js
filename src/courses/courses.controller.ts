d3878f5eccfd40cca8846b8f3735eef2d03a5667
Guilherme France <gart.2017@gmail.com>
2025-07-31 23:38:25 -0300

    first commit

---
+++ src/courses/courses.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get, Param, Post, Body, Res } from '@nestjs/common';
+
+
+@Controller('courses')
+export class CoursesController {
+  @Get()
+  findAll(@Res() response) {
+    return response.status(200).json({ message: 'Listagem de cursos' });
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `Curso com ID ${id}`;
+  }
+  @Post()
+  create(@Body() body) {
+    return body;
+  }
+}
